99faea7822699dcbabb8d600e25cc03ab92575b9
Anurag awasthi <33833603+amdollar@users.noreply.github.com>
2020-07-27 21:29:25 +0530

    AllPathsFromSourcetoTarget
    Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
    
    The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.

---
+++ Week4/AllPathsFromSourcetoTarget.java
@@ -0,0 +1,34 @@
+package io.test.Week4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPathsFromSourcetoTarget {
+
+	public static void main(String[] args) {
+		int[][] grap = new int[][] { { 1, 2 }, { 3 }, { 3 }, {} };
+	}
+
+	public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+		List<List<Integer>> res = new ArrayList<>();
+		List<Integer> path = new ArrayList<>();
+
+		path.add(0);
+		dfsSearch(graph, 0, res, path);
+
+		return res;
+	}
+
+	private void dfsSearch(int[][] graph, int node, List<List<Integer>> res, List<Integer> path) {
+		if (node == graph.length - 1) {
+			res.add(new ArrayList<Integer>(path));
+			return;
+		}
+
+		for (int nextNode : graph[node]) {
+			path.add(nextNode);
+			dfsSearch(graph, nextNode, res, path);
+			path.remove(path.size() - 1);
+		}
+	}
+}
