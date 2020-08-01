52ca1392f095833b877058681994c2c43de6159d
Anurag awasthi <33833603+amdollar@users.noreply.github.com>
2020-08-01 11:48:54 +0530

    WordBreakII
    Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.

---
+++ Week5/WordBreakII.java
@@ -0,0 +1,46 @@
+package io.test.Week5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordBreakII {
+
+	public static void main(String[] args) {
+		String s = "catsanddog";
+		List<String> wordDist = Arrays.asList("cat", "cats", "and", "sand", "dog");
+		List<String> dis = wordBreak(s, wordDist);
+		for (String str : dis)
+			System.out.println(str);
+	}
+
+	public static List<String> wordBreak(String s, List<String> wordDict) {
+		if (s.length() > 100)
+			return new ArrayList();
+		List<String> result = new ArrayList<String>();
+		wordBreakUtil(s, wordDict, result, new StringBuilder());
+		return result;
+	}
+
+	public static void wordBreakUtil(String s, List<String> wordDict, List<String> result, StringBuilder subList) {
+		// add " " between 2 words in subList
+		if (subList.length() != 0) {
+			subList.append(" ");
+		}
+		// iterate over all the words in wordDict
+		for (String word : wordDict) {
+			if (s.startsWith(word)) {
+				StringBuilder sb = new StringBuilder(subList);
+				// append current match in sb
+				sb.append(word);
+				// if this is last word to be matched
+				if (s.equals(word)) {
+					result.add(new String(sb));
+				} else {
+					wordBreakUtil(s.substring(word.length()), wordDict, result, sb);
+
+				}
+			}
+		}
+	}
+}
